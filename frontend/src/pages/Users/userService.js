87e11c478edeac7ab4ca48b04c785fb57b25a942
Syed Mohsin Ali <mohsin_1995@yahoo.com>
2020-04-02 23:38:17 +0500

    pagination to user section

--- frontend/src/pages/Users/userService.js
+++ frontend/src/pages/Users/userService.js
@@ -2,10 +2,7 @@ import Axios from "axios";
 import { baseURL } from "../../helper/systemConstants";
 
 export function GetAllUsers(params) {
-    // return Axios.get(`${baseURL}/users`, {
-    //     params
-    // })
-    return Axios.get(`http://localhost:5000/testproject-98a49/us-central1/users`, {
+    return Axios.get(`${baseURL}/users`, {
         params
     })
 }
